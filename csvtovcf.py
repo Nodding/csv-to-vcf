fbe0c196a0a6667fd9d8c95c51ce853216f8a7f1
Lucca Cioffi <32770553+Nodding@users.noreply.github.com>
2021-07-22 14:18:01 -0400

    Basic functionality, examples included

---
+++ csvtovcf.py
@@ -0,0 +1,47 @@
+import time
+import csv
+import io
+
+#Created by LUCCA CIOFFI.
+#Github: https://github.com/Nodding/csv-to-vcf
+
+print "Simple CSV to VCF"
+print "Make sure input.csv is in same directory."
+print "Must be formatted correctly!"
+
+outfile = open("output.vcf", "w")
+
+with open("input.csv", 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    num=0
+    for row in csv_reader:
+        outfile.writelines("BEGIN:VCARD" + "\n")
+        outfile.writelines("VERSION:2.1" + "\n")
+        name=""
+        cell=""
+        work=""
+        home=""
+        namelist=""
+        firstname=""
+        lastname=""
+        print row
+        name=row.get("Name")
+        cell=row.get("Cell")
+        work=row.get("Work")
+        home=row.get("Home")
+        print name + ", " + cell + ", " + work + ", " + home
+        namelist=name.split()
+        firstname=namelist[0]
+        lastname=namelist[1]
+        outfile.writelines("N:"+lastname+";"+firstname+";;;" + "\n")
+        outfile.writelines("FN:"+name + "\n")
+        outfile.writelines("TEL;CELL:"+cell + "\n")
+        outfile.writelines("TEL;WORK:"+work + "\n")
+        outfile.writelines("TEL;HOME:"+home + "\n")
+        outfile.writelines("END:VCARD" + "\n")
+        num += 1
+    print "Contacts Generated: " + str(num)
+outfile.close()
+   
+print "END!"
+end = raw_input()
